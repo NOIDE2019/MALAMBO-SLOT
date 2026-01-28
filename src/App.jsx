cee7b646883f1fa9f63d716a2cfea018fc927825
NOIDE2019 <malambotattoo@gmail.com>
2026-01-28 17:01:41 -0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div className="page">
+      <div className="card">
+        <h1>MALAMBO TATTOO</h1>
+        <p>La app está viva. Próximo paso: palanca + rodillos.</p>
+      </div>
+    </div>
+  );
+}
